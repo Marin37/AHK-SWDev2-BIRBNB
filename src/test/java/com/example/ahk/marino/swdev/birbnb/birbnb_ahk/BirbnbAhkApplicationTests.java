aa06f384071afaa238eee93f8e3e37ae7d2e8e2d
Marin37 <marijmotta@gmail.com>
2025-04-04 09:24:52 -0300

    spring boot first config

---
+++ src/test/java/com/example/ahk/marino/swdev/birbnb/birbnb_ahk/BirbnbAhkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahk.marino.swdev.birbnb.birbnb_ahk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirbnbAhkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
